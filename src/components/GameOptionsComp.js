45de159a952b7fbb2fafaf036d338f50106b1653
luiseer <85654418+luiseer@users.noreply.github.com>
2022-07-13 16:40:57 -0500

    Update game options

--- src/components/GameOptionsComp.js
+++ src/components/GameOptionsComp.js
@@ -1,26 +1,28 @@
 import { Row, Col, Button } from "react-bootstrap";
 
-const GameOptionsComp = ({ disks }) => {
+const GameOptionsComp = ({ disks, solve, reset, setDisks }) => {
   return (
     <Row>
       <Col>
         <span>Discos: {disks}</span>
-        <Button
-          variant="outline-secondary"
-        >
+        <Button variant="outline-secondary" style={{marginLeft: '20px', width: '40px'}} onClick={() => {if(disks < 8) setDisks(disks + 1)}}>
           +
         </Button>
         <Button
           variant="outline-secondary"
+          style={{marginLeft: '20px', width: '40px'}}
+          onClick={() => {
+            if (disks > 3) setDisks(disks - 1);
+          }}
         >
           -
         </Button>
       </Col>
       <Col>
-        <Button variant="outline-secondary" onClick={}>
+        <Button variant="outline-secondary"  onClick={() => reset()}>
           Reiniciar
         </Button>
-        <Button variant="outline-secondary" onClick={}>
+        <Button variant="outline-secondary" onClick={() => solve()}>
           Resolver
         </Button>
       </Col>
